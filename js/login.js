104bba1f52c43ab66f6d6665c9aa31311e37287a
Ahmed-M-Taha <132652114+Ahmed-M-Taha@users.noreply.github.com>
2023-10-19 19:18:50 +0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const correct_answer = "Admin";
+let useranswer = prompt("What Is The Passcode?");
+document.body.style.display = "none";
+if (useranswer == correct_answer) {
+    alert("Correct, Click Ok To Proceed");
+    window.location.href = "imAdmin.html";
+    document.body.style.display = "block";
+} else {
+    alert("Incorrect, Click Ok To Proceed");
+    window.location.href = "index.html";
+}
